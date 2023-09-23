00c2e8c72a35addb6313b70cab3264426d628729
kevinmedrano21 <kmedrano433@gmail.com>
2023-09-23 09:15:06 -0600

    implementacion del modal para actualizar

--- src/routers/index.js
+++ src/routers/index.js
@@ -19,13 +19,33 @@ router.post('/add', async(req, res) => {
     res.redirect('/');
 })
 
-router.get('/del/:id', async(req,res) => {
-    const {id} = req.params;
-    await Cliente.findByIdAndRemove(id);
-    res.redirect('/')
-})
+// router.get('/del/:id', async(req,res) => {
+//     const {id} = req.params;
+//     await Cliente.findByIdAndRemove(id);
+//     res.redirect('/')
+// })
 
+// router.get('/upd/:id', async(req,res) => {
+//     const {id} = req.params;
+//     await Cliente.findByIdAndUpdate(id);
+//     res.redirect('/')
+// })
 
+router.post('/upd/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, apellido, telefono } = req.body;
+    
+    try {
+        const cliente = await Cliente.findByIdAndUpdate(id, { nombre, apellido, telefono });
+        if (!cliente) {
+            return res.status(404).send("Registro no encontrado");
+        }
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error al actualizar el registro");
+    }
+});
 
 module.exports = router;
 
